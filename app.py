d0ca1690e518f7d3d8e4a1ca28636e3453678e35
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2020-06-05 12:51:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,78 @@
+# import flask
+# import pickle
+# import joblib
+
+# model = joblib.load('finalized_model.pkl')
+
+# app = flask.Flask(__name__, template_folder = 'templates')
+
+
+# @app.route('/', methods = ['GET', 'POST'])
+
+# def main():
+#     if flask.request.method = 'GET':
+#         return (flask.render_template('main.html'))
+    
+#     if flask.request.method = 'POST':
+        
+#         ID = flask.request.form ['ID']
+#         OCCUPANCY_DATE = flask.request.form ['OCCUPANCY_DATE']
+#         Existing Shelters In Area = flask.request.from ['Existing Shelters In Area']
+#         Max Temp (C) = flask.request.form ['Max Temp (C)']
+#         Min Temp (C) = flask.request.form ['Min Temp (C)']
+#         ORGANIZATION_NAME = flask.request.form ['ORGANIZATION_NAME']
+#         SHELTER_NAME = flask.requst.form ['SHELTER_NAME']
+#         SHELTER_ADDRESS = flask.request.form ['SHELTER_ADDRESS']
+#         SHELTER_CITY = flask.request.form['SHELTER_CITY']
+#         SHELTER_PROVINCE = flask.request.from ['SHELTER_PROVINCE']
+#         SHELTER_POSTAL_CODE = flask.request.form ['SHELTER_POSTAL_CODE']
+#         FACILITY_NAME = flask.request.form ['FACILITY_NAME']
+#         PROGRAM_NAME = flask.request.form['PROGRAM_NAME']
+#         SECTOR = flask.request.form ['SECTOR']
+# #         OCCUPANCY = flask.request.form ['OCCUPANCY']
+#         CAPACITY = flask.request.forn['CAPACITY']
+                                                             
+        
+#         input_variables = pd.DataFrame ([[ID,OCCUPANCY_DATE,Existing Shelters In Area,
+#                                           Max Temp (C),Min Temp (C),ORGANIZATION_NAME,
+#                                           SHELTER_NAME,SHELTER_ADDRESS,SHELTER_CITY,
+#                                           SHELTER_PROVINCE,SHELTER_POSTAL_CODE,
+#                                           FACILITY_NAME,PROGRAM_NAME,SECTOR,
+#                                           OCCUPANCY,CAPACITY]], columns = ['ID','OCCUPANCY_DATE',
+#                                                                                 'Existing Shelters In Area',
+#                                                                                 'Max Temp (C)','Min Temp (C)',
+#                                                                                 'ORGANIZATION_NAME',
+#                                                                                 'SHELTER_NAME',
+#                                                                                 'SHELTER_ADDRESS',
+#                                                                                 'SHELTER_CITY',
+#                                                                                 'SHELTER_PROVINCE',
+#                                                                                 'SHELTER_POSTAL_CODE',
+#                                                                                 'FACILITY_NAME',
+#                                                                                 'PROGRAM_NAME' ,
+#                                                                                 'SECTOR',
+#                                                                                 'CAPACITY'])
+#         prediction = model.predict (input_variables)[0]
+#         return(flask.render_template('main.html', orginal_input = ))
+
+
+# if __name__ == '__name__':
+#     app.run()
+
+
+
+
+    
+# # cols = ['ID','OCCUPANCY_DATE','Existing Shelters In Area','Max Temp (C)','Min Temp (C)','ORGANIZATION_NAME','SHELTER_NAME','SHELTER_ADDRESS','SHELTER_CITY','SHELTER_PROVINCE','SHELTER_POSTAL_CODE','FACILITY_NAME','PROGRAM_NAME' ,'SECTOR' ,'OCCUPANCY' ,'CAPACITY']
+
+    
+    
+    
+    
+    
+    
+    
+    
+
 from flask import Flask,request, url_for, redirect, render_template, jsonify
 from pycaret.regression import *
 import pandas as pd
@@ -17,7 +92,9 @@ app = Flask(__name__)
 model = load_model('finalized_model')
 # cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
-cols = ['ID','OCCUPANCY_DATE','Existing Shelters In Area','Max Temp (C)','Min Temp (C)','ORGANIZATION_NAME','HELTER_NAME','SHELTER_ADDRESS','SHELTER_CITY','SHELTER_PROVINCE','SHELTER_POSTAL_CODE','FACILITY_NAME','PROGRAM_NAME' ,'SECTOR' ,'OCCUPANCY' ,'CAPACITY']
+cols = ['ID','OCCUPANCY_DATE','Existing Shelters In Area','Max Temp (C)','Min Temp (C)','ORGANIZATION_NAME','SHELTER_NAME','SHELTER_ADDRESS','SHELTER_CITY','SHELTER_PROVINCE','SHELTER_POSTAL_CODE','FACILITY_NAME','PROGRAM_NAME' ,'SECTOR' ,'CAPACITY']
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -35,7 +112,7 @@ def predict():
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
-    prediction = predict_model(model, data=data_unseen)
+    prediction = model.predict(data=data_unseen)
     output = prediction.Label[0]
     return jsonify(output)
 
